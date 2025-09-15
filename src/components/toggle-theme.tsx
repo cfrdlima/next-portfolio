38a744974c6749f65cf37a559ae52d2cb24b997b
claudinei <claudinei@mertins.com>
2025-09-15 19:50:55 -0300

    feat: integrate theme provider and toggle functionality
    - Added ThemeProvider component to manage theme state using next-themes.
    - Implemented ThemeToggle component for switching between light and dark themes.
    - Updated layout to include ThemeProvider and suppress hydration warnings.
    - Refactored Home page to utilize ThemeToggle and removed unnecessary content.
    - Enhanced global styles with new CSS variables for theming and animations.
    - Introduced Radix UI dropdown menu components for better UI interactions.
    - Added utility functions for class name management with clsx and tailwind-merge.
    - Updated package.json to include new dependencies for theming and UI components.

---
+++ src/components/toggle-theme.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { FaCloudMoon, FaCloudSun } from "react-icons/fa";
+import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return null;
+
+  return (
+    <>
+      <Button
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        className="cursor-pointer rounded-full"
+      >
+        {theme === "dark" ? (
+          <FaCloudSun size={32} />
+        ) : (
+          <FaCloudMoon size={32} />
+        )}
+      </Button>
+    </>
+  );
+}
